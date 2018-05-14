e1db7e5104bd6c7612ed72b9426bd51d5164b553
Corneezius <cfr2116@columbia.edu>
2018-05-14 10:47:52 -0400

    add imagelist component

---
+++ client/components/image_list.js
@@ -0,0 +1,15 @@
+// Import React
+import React from "react";
+
+
+// Create component
+const ImageList = () = > {
+  return (
+    <ul>
+      <li> Image Detail</li>
+    </ul>
+  );
+};
+
+// Export Component
+export default ImageList;
