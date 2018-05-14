07b48d319fe58a03532f4f38664193219ce29043
Corneezius <cfr2116@columbia.edu>
2018-05-14 11:06:12 -0400

    update image detail component and add dummy images

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,12 +1,19 @@
 // Import React
 import React from "react";
+import ImageDetail from "./image_detail";
 
+
+const IMAGES = [
+  {title: "One", link:"https://dummyimage.com/600x400/000/fff&text=Hello"},
+  {title: "Two", link:"https://dummyimage.com/600x400/000/fff&text=Hello"},
+  {title: "Three",link:"https://dummyimage.com/600x400/000/fff&text=Hello"},
+]
 
 // Create component
-const ImageList = () = > {
+const ImageList = () => {
   return (
     <ul>
-      <li> Image Detail</li>
+      <ImageDetail />
     </ul>
   );
 };
