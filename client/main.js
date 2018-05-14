62f276c9ea8d5f704bac06ba25db791a45bc1505
Corneezius <cfr2116@columbia.edu>
2018-05-14 12:49:57 -0400

    make ajax call to Rick and Morty data

--- client/main.js
+++ client/main.js
@@ -1,18 +1,25 @@
 // import React
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from "react-dom";
+import axios from "axios";
 import ImageList from "./components/image_list";
 
+
+
 // create component
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ImageList />
+      </div>
+    );
+  }
 };
 
 // render component
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector(".container"));
+  axios.get("https://rickandmortyapi.com/api/episode/")
+    .then(response => console.log(response));
 });
