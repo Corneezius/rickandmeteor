c09d21eb2ce286bf0f9ba0e416c22dc5a2580e37
Corneezius <cfr2116@columbia.edu>
2018-05-14 13:03:54 -0400

    update state

--- client/main.js
+++ client/main.js
@@ -8,7 +8,17 @@ import ImageList from "./components/image_list";
 
 // create component
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {images: [] };
+  }
+  componentWillMount(){
+    axios.get("https://rickandmortyapi.com/api/episode/")
+      .then(response => this.setState({images: response.data.results}));
+  }
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <ImageList />
@@ -20,6 +30,4 @@ class App extends Component {
 // render component
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector(".container"));
-  axios.get("https://rickandmortyapi.com/api/episode/")
-    .then(response => console.log(response));
 });
