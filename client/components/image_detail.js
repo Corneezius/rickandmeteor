fa92323bf93de02a58624a573ffed6b46572d5f4
Corneezius <cfr2116@columbia.edu>
2018-05-14 13:23:07 -0400

    update character status

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -4,14 +4,13 @@ const ImageDetail = (props) => {
   return (
     <li className="media list-group-item">
       <div className="media-left">
-        <h4 className="media-heading">
-          {props.image.name}
-        </h4>
+        <img src={props.image.image} />
       </div>
       <div className="media-body">
-        <h4 className="media-heading">
-          <img src={props.image.image} />
-        </h4>
+        <h3 className="media-heading">
+          {props.image.name}
+        </h3>
+        <h4> {props.image.status} </h4>
       </div>
     </li>
   );
